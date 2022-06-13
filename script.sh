f3b4d9419a3d0f7fb28f70c54e5b350bdb174a88
marta <martampm.94@gmail.com>
2022-06-14 00:02:34 +0100

    initial commit

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -r fol_1
+rm -r fol_2
+rm .gitignore
+mkdir fol_1
+mkdir fol_2
+touch fol_1/1_1.txt
+touch fol_1/1_2.txt
+touch fol_1/1_3.txt
+touch fol_2/2_1.txt
+touch fol_2/2_2.txt
+touch fol_2/2_3.txt
+touch .gitignore
+echo fol_1 >> .gitignore
+echo fol_2 >> .gitignore
