e30c6bdb79fbf9347e4aebc25b6182a91058e926
marta <martampm.94@gmail.com>
2022-06-14 00:23:35 +0100

    fixed bash 2

--- script.sh
+++ script.sh
@@ -13,3 +13,10 @@ touch fol_2/2_3.txt
 touch .gitignore
 echo fol_1 >> .gitignore
 echo fol_2 >> .gitignore
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_2/2_2.txt
+echo "Job completed"
